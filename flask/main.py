28cc01d93ab70a50c8c946ae623e05e4118a668d
mgolu <mgoluch02@gmail.com>
2024-04-26 12:32:01 +0200

    zaloguj i wyloguj

--- flask/main.py
+++ flask/main.py
@@ -1,30 +1,49 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for, session
 from neo4j import GraphDatabase
+import re
 
 app = Flask(__name__)
+app.secret_key = 'super tajny klucz'  # Klucz sesji
 
 class Neo4jService(object):
 
     def __init__(self, uri, user, password):
         self._driver = GraphDatabase.driver(uri, auth=(user, password))
 
-    def create_user(self, login, haslo, id_uzytkownika):
-        print('coś')
-        with self._driver.session() as session:
-            session.write_transaction(self._create_user, login, haslo, id_uzytkownika)
+    def create_user(self, email, haslo):
+        if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+            return "Niepoprawny adres e-mail jako login"
+        if not re.search(r"\d", haslo) or not re.search(r"[a-zA-Z]", haslo):
+            return "Hasło musi zawierać zarówno litery i cyfry"
 
-    def get_last_user_id(self):
         with self._driver.session() as session:
-            result = session.run("MATCH (u:Użytkownik) RETURN max(u.id_użytkownika) as last_id")
-            last_id = result.single()['last_id']
-            return last_id
+            session.write_transaction(self._create_user, email, haslo)
 
-    @staticmethod
-    def _create_user(tx, login, haslo, id_uzytkownika):
+    def check_user(self, email, haslo):
+        with self._driver.session() as session:
+            result = session.run("MATCH (u:Użytkownik {login: $email, hasło: $haslo}) RETURN u", email=email, haslo=haslo)
+            return bool(result.single())
+
+    def get_user_id(self, email):
+        with self._driver.session() as session:
+            result = session.run("MATCH (u:Użytkownik {login: $email}) RETURN u.id_użytkownika as id", email=email)
+            record = result.single()
+            if record:
+                return record['id']
+            return None
+
+    def _create_user(self, tx, email, haslo):
+        last_id = self.get_last_user_id(tx)
+        new_id = int(last_id) + 1 if last_id else 1
         query = (
-            "CREATE (u:Użytkownik {login: $login, hasło: $haslo, id_użytkownika: $id_uzytkownika})"
+            "CREATE (u:Użytkownik {login: $email, hasło: $haslo, id_użytkownika: $id_uzytkownika})"
         )
-        tx.run(query, login=login, haslo=haslo, id_uzytkownika=id_uzytkownika)
+        tx.run(query, email=email, haslo=haslo, id_uzytkownika=new_id)
+
+    def get_last_user_id(self, tx):
+        result = tx.run("MATCH (u:Użytkownik) RETURN max(u.id_użytkownika) as last_id")
+        last_id = result.single()['last_id']
+        return last_id
 
 # Konfiguracja Neo4j
 NEO4J_URI = "neo4j+s://eda19c51.databases.neo4j.io"
@@ -35,27 +54,39 @@ neo4j_service = Neo4jService(NEO4J_URI, NEO4J_USER, NEO4J_PASSWORD)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    if 'logged_in' in session:
+        #print("Zalogowany")
+        return render_template('index2.html', logged_in=True)
+    #print("Nie zalogowany")
+    return render_template('index.html', logged_in=False)
 
 
 @app.route('/register', methods=['POST'])
 def register():
-    login = request.form['login']
+    email = request.form['email']
     haslo = request.form['haslo']
 
-    # Pobranie ostatniego ID użytkownika i dodanie 1
-    last_id = neo4j_service.get_last_user_id()
-    new_id = int(last_id) + 1
+    result = neo4j_service.create_user(email, haslo)
+    if result:
+        return result
 
-    # Dodanie użytkownika do bazy danych
-    neo4j_service.create_user(login, haslo, new_id)
+    return redirect(url_for('login'))
 
-    # Przekazanie nowego ID do szablonu
-    return render_template('success.html', login=login, id_uzytkownika=new_id)
+@app.route('/login', methods=['POST'])
+def login():
+    email = request.form['email']
+    haslo = request.form['haslo']
 
+    if neo4j_service.check_user(email, haslo):
+        session['logged_in'] = True
+        return redirect(url_for('index'))
+    else:
+        return "Niepoprawny login lub hasło"
 
-
-
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
